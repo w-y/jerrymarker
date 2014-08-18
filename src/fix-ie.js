d39086cb280838ff63c56140cad50d3e59e8fc73
w-y <wang.wang.yue@qq.com>
2014-08-18 03:13:11 +0000

    fix:fix ie getPrototypeOf bug

---
+++ src/fix-ie.js
@@ -0,0 +1,11 @@
+/* jshint ignore:start */
+if (typeof Object.getPrototypeOf !== "function")
+Object.getPrototypeOf = ''.__proto__ === String.prototype
+? function (object) {
+    return object.__proto__;
+}
+: function (object) {
+    // May break if the constructor has been tampered with
+    return object.constructor.prototype;
+}
+/* jshint ignore:end */
