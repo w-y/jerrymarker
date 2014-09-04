742042a6ba0bef022b40f89176c165ec77a92048
w-y <wang.wang.yue@qq.com>
2014-09-04 02:55:46 +0000

    fix: ie bug: Object.create not avaliable in IE

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,7 +60,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-sed');
 
   grunt.loadTasks('tasks');
 
--- src/fix-ie.js
+++ src/fix-ie.js
@@ -8,4 +8,17 @@ Object.getPrototypeOf = ''.__proto__ === String.prototype
     // May break if the constructor has been tampered with
     return object.constructor.prototype;
 }
+if (!Object.create) {
+    Object.create = (function(){
+        function F(){}
+
+        return function(o){
+            if (arguments.length != 1) {
+                throw new Error('Object.create implementation only accepts one parameter.');
+            }
+            F.prototype = o;
+            return new F()
+        }
+    })()
+}
 /* jshint ignore:end */
